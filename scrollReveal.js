06fad6f630f410038e69bac505a889e6bf99e82f
yaronrueger <yaron.rueger@gmx.de>
2022-12-08 08:45:35 +0100

    new javascript file

---
+++ scrollReveal.js
@@ -0,0 +1,60 @@
+ /* ScrollReveal allg. Einstellungen */
+ ScrollReveal({
+    reset: false,
+    distance: "70px",
+    duration: 2500,
+    delay: 100,
+  });
+
+  /* genaue Definierung der einzelnen Divs */
+  ScrollReveal().reveal(".menu", {
+    delay: 150,
+    origin: "top",
+  });
+
+  ScrollReveal().reveal(".mainTitle, .sectionTitle", {
+    delay: 150,
+    origin: "left",
+  });
+
+  ScrollReveal().reveal(".firstSection .imageMain", {
+    delay: 150,
+    origin: "bottom",
+  });
+
+  ScrollReveal().reveal(".infoBox", {
+    delay: 200,
+    origin: "right",
+  });
+
+  ScrollReveal().reveal(".secondSection .image", {
+    delay: 150,
+    origin: "bottom",
+  });
+
+  ScrollReveal().reveal(".infoBox2", {
+    delay: 150,
+    origin: "right",
+  });
+
+  /**/
+  ScrollReveal().reveal(".thirdSection .image",{
+    delay: 145,
+    origin: "left",
+  });
+
+  ScrollReveal().reveal(".fourthSection, .image", {
+    delay: 145,
+    origin: "bottom",
+  });
+
+  ScrollReveal().reveal(".infoMemorie", {
+    delay: 145,
+    origin: "right",
+  });
+
+  ScrollReveal().reveal(".mediaInfo li", {
+    delay: 100,
+    origin: "top",
+    interval: 50,
+  });
