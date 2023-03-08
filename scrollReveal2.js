1f4e53eeb7de5b8b3712ce179b854a3ce5dac1c7
yrueg <yaron.rueger@gmx.de>
2023-03-08 15:39:52 +0100

    fixed newsslider

---
+++ scrollReveal2.js
@@ -0,0 +1,12 @@
+ /* ScrollReveal allg. Einstellungen  
+ ScrollReveal({
+    reset: false,
+    distance: "1px",
+    duration: 2500,
+    delay: 50,
+  });
+
+ScrollReveal().reveal(".slider-container, .slideButton", {
+    delay: -10,
+    origin: "right",
+  });*/
